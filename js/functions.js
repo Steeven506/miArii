6cbdb68e50b836178fc005005b63726fa23c460b
Steeven506 <sjchaconm@ufpso.edu.co>
2023-12-04 19:33:20 -0500

    index correction and add myStar

--- js/functions.js
+++ js/functions.js
@@ -12,3 +12,13 @@ button.addEventListener('click',()=>{
 
 })
 
+/*my star*/
+document.getElementById('next').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').prepend(lists[lists.length = 6]);
+}
+
